83a318d6b61ab1c6d7df6f97315b89392344804f
MiguelAngelSanJim <159305124+MiguelAngelSanJim@users.noreply.github.com>
2024-11-19 20:59:47 +0100

    Update app.js
    assasa

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-  // Obtener las entidades de los productos
+document.addEventListener("DOMContentLoaded", function () {
   const product1 = document.getElementById("product1");
   const product2 = document.getElementById("product2");
   const product3 = document.getElementById("product3");
 
-  // Botones para seleccionar el producto
   const btn1 = document.getElementById("btn1");
   const btn2 = document.getElementById("btn2");
   const btn3 = document.getElementById("btn3");
 
-  // Agregar eventos a los botones para mostrar los productos
-  btn1.addEventListener("click", function() {
-    // Ocultar los otros productos y mostrar el producto 1
-    product1.setAttribute("visible", "true");
-    product2.setAttribute("visible", "false");
-    product3.setAttribute("visible", "false");
-  });
-
-  btn2.addEventListener("click", function() {
-    // Ocultar los otros productos y mostrar el producto 2
-    product1.setAttribute("visible", "false");
-    product2.setAttribute("visible", "true");
-    product3.setAttribute("visible", "false");
-  });
-
-  btn3.addEventListener("click", function() {
-    // Ocultar los otros productos y mostrar el producto 3
+  function showModel(modelToShow) {
     product1.setAttribute("visible", "false");
     product2.setAttribute("visible", "false");
-    product3.setAttribute("visible", "true");
+    product3.setAttribute("visible", "false");
+
+    modelToShow.setAttribute("visible", "true");
+  }
+
+  btn1.addEventListener("click", function () {
+    showModel(product1);
+  });
+
+  btn2.addEventListener("click", function () {
+    showModel(product2);
+  });
+
+  btn3.addEventListener("click", function () {
+    showModel(product3);
   });
 });
